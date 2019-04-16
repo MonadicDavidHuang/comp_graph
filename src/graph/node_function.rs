19079acefa31bfc191427dd90ecfd389ef2c9eec
MonadicDavidHuang <mynameistoyoko@gmail.com>
2019-04-17 02:35:15 +0900

    Refactor.

--- src/graph/node_function.rs
+++ src/graph/node_function.rs
@@ -9,7 +9,7 @@ use self::ndarray::*;
 pub trait CgFunction {
     fn forward(&self) -> Array2<f32>;
     // fn backward(&self) -> ();
-    fn set_child(&mut self, chi_v: Arc<RwLock<CgVariable>>) -> ();
+    fn set_child(&mut self, chi_v: Arc<RwLock<CgVariable>>);
     fn get_domain_shape(&self) -> (usize, usize);
     fn get_codomain_shape(&self) -> (usize, usize);
 }
--- src/graph/node_functions.rs
+++ src/graph/node_functions.rs
@@ -11,103 +11,121 @@ use self::ndarray_linalg::*;
 
 // ++++++++++++++++++++++++ CgPlus ++++++++++++++++++++++++ //
 pub struct CgPlus {
-    dom_shape: (usize, usize),
-    cod_shape: (usize, usize),
-    par_v_left: Arc<RwLock<CgVariable>>,
-    par_v_right: Arc<RwLock<CgVariable>>,
-    chi_v_opt: Option<Weak<RwLock<CgVariable>>>,
+    domain_shape: (usize, usize),
+    codomain_shape: (usize, usize),
+    left_parent_reference: Arc<RwLock<CgVariable>>,
+    right_parent_reference: Arc<RwLock<CgVariable>>,
+    child_variable_reference_weak_optional: Option<Weak<RwLock<CgVariable>>>,
 }
 
 impl CgPlus {
-    pub fn new(par_v_left: Arc<RwLock<CgVariable>>,
-               par_v_right: Arc<RwLock<CgVariable>>)
+    pub fn new(left_parent_reference: Arc<RwLock<CgVariable>>,
+               right_parent_reference: Arc<RwLock<CgVariable>>)
                -> Arc<RwLock<CgPlus>> {
-        let shape_left = (*(*par_v_left).read().unwrap()).get_shape();
-        let shape_right = (*(*par_v_right).read().unwrap()).get_shape();
+        let shape_left = (*(*left_parent_reference).read().unwrap()).get_shape();
+        let shape_right = (*(*right_parent_reference).read().unwrap()).get_shape();
+
         assert_eq!(shape_left, shape_right);
 
-        let obj_data = CgPlus {
-            dom_shape: shape_left,
-            cod_shape: shape_left, // `+` returns same shape
-            par_v_left: par_v_left,
-            par_v_right: par_v_right,
-            chi_v_opt: None,
+        let domain_shape = shape_left;
+        let codomain_shape = shape_left;
+        let child_variable_reference_weak_optional = None;
+
+        let data = CgPlus {
+            domain_shape,
+            codomain_shape, // `+` returns same shape
+            left_parent_reference,
+            right_parent_reference,
+            child_variable_reference_weak_optional,
         };
 
-        let ref_data = Arc::new(RwLock::new(obj_data));
-        ref_data
+        let reference = Arc::new(RwLock::new(data));
+        reference
     }
 }
 
 impl CgFunction for CgPlus {
     fn forward(&self) -> Array2<f32> {
-        let mut guard_left = (*self.par_v_left).write().unwrap();
-        let mut guard_right = (*self.par_v_right).write().unwrap();
-        let dom_left_ref = (*guard_left).forward();
-        let dom_right_ref = (*guard_right).forward();
-        let cod_obj = dom_left_ref + dom_right_ref;
-        cod_obj
+        let mut guard_left = (*self.left_parent_reference).write().unwrap();
+        let mut guard_right = (*self.right_parent_reference).write().unwrap();
+        let left_domain_reference = (*guard_left).forward();
+        let right_domain_reference = (*guard_right).forward();
+        let codomain_data = left_domain_reference + right_domain_reference;
+        codomain_data
     }
 
     // fn backward(&self) -> () {}
 
-    fn set_child(&mut self, chi_v: Arc<RwLock<CgVariable>>) -> () {
-        let weak_chi_v: Weak<RwLock<CgVariable>> = Arc::downgrade(&chi_v);
-        self.chi_v_opt = Some(weak_chi_v);
+    fn set_child(&mut self, child_variable_reference: Arc<RwLock<CgVariable>>) {
+        let child_variable_reference_weak =
+            Arc::downgrade(&child_variable_reference);
+        self.child_variable_reference_weak_optional = Some(child_variable_reference_weak);
     }
 
-    fn get_domain_shape(&self) -> (usize, usize) { self.dom_shape }
-    fn get_codomain_shape(&self) -> (usize, usize) { self.cod_shape }
+    fn get_domain_shape(&self) -> (usize, usize) { self.domain_shape }
+    fn get_codomain_shape(&self) -> (usize, usize) { self.codomain_shape }
 }
 // ++++++++++++++++++++++++ /CgPlus ++++++++++++++++++++++++ //
 
 // ++++++++++++++++++++++++ CgMse ++++++++++++++++++++++++ //
 pub struct CgMse {
-    dom_shape: (usize, usize),
-    cod_shape: (usize, usize),
-    par_v_left: Arc<RwLock<CgVariable>>,
-    par_v_right: Arc<RwLock<CgVariable>>,
-    chi_v_opt: Option<Weak<RwLock<CgVariable>>>,
+    domain_shape: (usize, usize),
+    codomain_shape: (usize, usize),
+    left_parent_reference: Arc<RwLock<CgVariable>>,
+    right_parent_reference: Arc<RwLock<CgVariable>>,
+    child_variable_reference_weak_optional: Option<Weak<RwLock<CgVariable>>>,
 }
 
 impl CgMse {
-    pub fn new(par_v_left: Arc<RwLock<CgVariable>>,
-               par_v_right: Arc<RwLock<CgVariable>>) -> Arc<RwLock<CgMse>> {
-        let shape_left = (*(*par_v_left).read().unwrap()).get_shape();
-        let shape_right = (*(*par_v_right).read().unwrap()).get_shape();
+    pub fn new(left_parent_reference: Arc<RwLock<CgVariable>>,
+               right_parent_reference: Arc<RwLock<CgVariable>>)
+               -> Arc<RwLock<CgMse>> {
+        let shape_left = (*(*left_parent_reference).read().unwrap()).get_shape();
+        let shape_right = (*(*right_parent_reference).read().unwrap()).get_shape();
+
         assert_eq!(shape_left, shape_right);
 
-        let obj_data = CgMse {
-            dom_shape: shape_left,
-            cod_shape: shape_left, // `+` returns same shape
-            par_v_left: par_v_left,
-            par_v_right: par_v_right,
-            chi_v_opt: None,
+        let domain_shape = shape_left;
+        let codomain_shape = (1 as usize, 1 as usize); // Mse is always scalar
+        let child_variable_reference_weak_optional = None;
+
+        let data = CgMse {
+            domain_shape,
+            codomain_shape,
+            left_parent_reference,
+            right_parent_reference,
+            child_variable_reference_weak_optional,
         };
 
-        let ref_data = Arc::new(RwLock::new(obj_data));
-        ref_data
+        let reference = Arc::new(RwLock::new(data));
+        reference
     }
 }
 
 impl CgFunction for CgMse {
     fn forward(&self) -> Array2<f32> {
-        let mut guard_left = (*self.par_v_left).write().unwrap();
-        let mut guard_right = (*self.par_v_right).write().unwrap();
-        let dom_left_ref = (*guard_left).forward();
-        let dom_right_ref = (*guard_right).forward();
-        let cod_obj = dom_left_ref + dom_right_ref;
-        cod_obj
+        let mut left_guard = (*self.left_parent_reference).write().unwrap();
+        let mut right_guard = (*self.right_parent_reference).write().unwrap();
+        let left_domain_ref = (*left_guard).forward();
+        let right_domain_ref = (*right_guard).forward();
+        let codomain_data = left_domain_ref + right_domain_ref;
+        codomain_data
     }
 
     // fn backward(&self) -> () {}
 
-    fn set_child(&mut self, chi_v: Arc<RwLock<CgVariable>>) -> () {
-        let weak_chi_v: Weak<RwLock<CgVariable>> = Arc::downgrade(&chi_v);
-        self.chi_v_opt = Some(weak_chi_v);
+    fn set_child(&mut self, child_variable_reference: Arc<RwLock<CgVariable>>) {
+        let child_variable_reference_weak =
+            Arc::downgrade(&child_variable_reference);
+        self.child_variable_reference_weak_optional = Some(child_variable_reference_weak);
     }
 
-    fn get_domain_shape(&self) -> (usize, usize) { self.dom_shape }
-    fn get_codomain_shape(&self) -> (usize, usize) { self.cod_shape }
+    fn get_domain_shape(&self) -> (usize, usize) {
+        self.domain_shape
+    }
+
+    fn get_codomain_shape(&self) -> (usize, usize) {
+        self.codomain_shape
+    }
 }
 // ++++++++++++++++++++++++ /CgMse ++++++++++++++++++++++++ //
